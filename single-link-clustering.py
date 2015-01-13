4b0125a76f77cff679b13e0ae512ae9dd1dd543e
compliant <compliant@users.noreply.github.com>
2015-01-13 12:58:55 +0100

    Use the functions in coassociation.py
    Compute the distance matrix instead of using a precomputed one.

--- single-link-clustering.py
+++ single-link-clustering.py
@@ -1,9 +1,11 @@
 import testdata
+import coassociation
 from sklearn import cluster
 
 agglomerative = cluster.AgglomerativeClustering(n_clusters = 3,
                                                 affinity = 'precomputed',
                                                 linkage = 'average')
 
-agglomerative.fit(testdata.distancematrix)
+distancematrix = coassociation.dict_to_matrix(coassociation.distance_matrix(testdata.data))
+agglomerative.fit(distancematrix)
 print(agglomerative.labels_)

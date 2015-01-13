794bcdc8501389eb73b4ed22ccdd98b4a29e31f0
Abhinav-baijal <abhinav.baijal@yahoo.com>
2015-01-13 12:10:29 +0530

    for changing the existing data format to a simple matrix format for co-association matrix

---
+++ co-association.py
@@ -0,0 +1,14 @@
+def convert_data(data_orignal)
+	result={}
+	
+	for clustering,value in data.values()
+		result[clustering]={}
+		for k,v in clustering:
+			for image in v:
+				result [clustering][image]=k
+			
+	return result
+
+	
+	
+	

15e470c68af85c2e4cc099dcd28d931926ed8902
compliant <compliant@users.noreply.github.com>
2015-01-20 14:50:21 +0100

    Add function image_utils.images_on_grid
    Build a grid of thumbnails from image paths.

--- image_utils.py
+++ image_utils.py
@@ -1,4 +1,24 @@
+from PIL import Image
+import data
 import images
+import math
 
 def image_ids_to_paths(ids):
     return ['images/' + images.images[id][1] for id in ids]
+
+def images_on_grid(paths):
+    thumbnail_size = 100
+    count_x = math.ceil(math.sqrt(len(paths)))
+    count_y = math.floor(math.sqrt(len(paths)))
+
+    result = Image.new('RGB', (count_x*thumbnail_size,count_y*thumbnail_size))
+
+    for i in range(len(paths)):
+        x = i % count_x
+        y = i // count_x
+
+        image = Image.open(paths[i])
+        image.thumbnail((thumbnail_size, thumbnail_size))
+        result.paste(image, (x * thumbnail_size, y * thumbnail_size))
+
+    return result

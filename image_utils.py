38a8f7f6b047a6d38ed16f9dd80c6b723f9b71ba
compliant <compliant@users.noreply.github.com>
2015-01-20 15:00:30 +0100

    Change thumbnail size
    100×100 → 200×200

--- image_utils.py
+++ image_utils.py
@@ -7,7 +7,7 @@ def image_ids_to_paths(ids):
     return ['images/' + images.images[id][1] for id in ids]
 
 def images_on_grid(paths):
-    thumbnail_size = 100
+    thumbnail_size = 200
     count_x = math.ceil(math.sqrt(len(paths)))
     count_y = math.floor(math.sqrt(len(paths)))
 

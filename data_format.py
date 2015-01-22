508dc8187e4671a85d3696ed89f66d1819f93460
compliant <compliant@users.noreply.github.com>
2015-01-22 12:51:41 +0100

    Add labels_as_dictionary
    Add the image ids as keys to a list of labels ordered by image id.

--- data_format.py
+++ data_format.py
@@ -1,3 +1,5 @@
+import images
+
 def dict_to_list(d):
     tmp_dict = {}
     for cluster,images in d.items():
@@ -29,3 +31,6 @@ def dict_to_matrix(d):
             subresult.append(d[k][k2])
         result.append(subresult)
     return result
+
+def labels_as_dictionary(labels):
+    return dict(zip(images.imageids, labels))

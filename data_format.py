a1921859550e762afc952842af0ac81c8803db6c
compliant <compliant@users.noreply.github.com>
2015-01-22 12:50:32 +0100

    Add dict_to_list and list_to_dict
    dict_to_list converts a dictionary-like clustering to a list of
    labels (sorted using the image id). list_to_dict performs the reverse
    operation.

--- data_format.py
+++ data_format.py
@@ -1,3 +1,26 @@
+def dict_to_list(d):
+    tmp_dict = {}
+    for cluster,images in d.items():
+        for image in images:
+            tmp_dict[image] = cluster
+
+    result = []
+    for k in sorted(tmp_dict):
+        result.append(tmp_dict[k])
+
+    return result
+
+def list_to_dict(l):
+    result = {}
+    for cluster in l:
+        result[cluster] = []
+
+    d = labels_as_dictionary(l)
+    for image,cluster in d.items():
+        result[cluster].append(image)
+
+    return result
+
 def dict_to_matrix(d):
     result = []
     for k in sorted(d):

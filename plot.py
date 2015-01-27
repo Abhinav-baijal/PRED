8c9d33f481ce8d3e869ba98211c12c8ff3bcfa05
compliant <compliant@users.noreply.github.com>
2015-01-27 16:11:50 +0100

    Add plot.bar_chart

--- plot.py
+++ plot.py
@@ -6,3 +6,7 @@ def consensus_quality_bar_chart(consensus_quality, original_data_quality):
                width = 0.6,
                color = ['red'] + ['blue']*len(original_data_quality))
     pyplot.show()
+
+def bar_chart(left,height):
+    pyplot.bar(left=left,height=height)
+    pyplot.show()

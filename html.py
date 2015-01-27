4315400baed7f5854440ecd7fe817f933962a6b1
compliant <compliant@users.noreply.github.com>
2015-01-27 16:51:01 +0100

    Make links of the images in html documents

--- html.py
+++ html.py
@@ -2,18 +2,18 @@ def write_html(clustering):
 
     def display_images(ids):
         import image_utils
-        paths=image_utils.image_ids_to_paths(ids)
-        for i in paths:
-            file.write('<img src="' + i + '" height="100">')
-           
+        paths = dict(zip(ids, image_utils.image_ids_to_paths(ids)))
+        for id,path in paths.items():
+            file.write('<a href="' + path + '"><img src="' + path + '" alt="Image ' + str(id) + '" height="100"></a>\n')
+
     def display_clusters(clusters):
         for k,v in clusters.items():
             file.write('<h1>'+ str(k) + '</h1>')
-            
+
             display_images(v)
-            
+
     file =open('display.html', 'w')
-                 
+
     file.write(
     """<!DOCTYPE html>
     <html>
@@ -31,6 +31,5 @@ def write_html(clustering):
     file.write(
     """</body>
     </html>""")
-    
+
     file.close()
-    
